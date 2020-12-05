c3f725c8d05082b4b06a65d9c3580a17290495f7
Max Mitrani <mjmitrani168@gmail.com>
2020-12-05 16:42:53 -0700

    Add functional Person endpoint. Test passing

--- app/controllers/api/people_controller.rb
+++ app/controllers/api/people_controller.rb
@@ -1,11 +1,14 @@
 class Api::PeopleController < ApplicationController
   
     def index
-         people = PersonSerializer.new.serialize(Person.all)
+         people = PersonSerializer.new.people_serialize(Person.all)
         render json: {data: people}
     end
 
     def show
+        person_data = Person.find_by(last: params['last'], first: params['first'])
+        person = PersonSerializer.new.person_serialize(person_data)
+        render json: {data: person}
     end
 
 end
