e6eb64f5bf6208ece0401c06ae73cfbc4e9bd0bb
Max Mitrani <mjmitrani168@gmail.com>
2020-12-05 16:16:41 -0700

    Add  single Person endpoint testing

--- app/controllers/api/people_controller.rb
+++ app/controllers/api/people_controller.rb
@@ -1,6 +1,11 @@
 class Api::PeopleController < ApplicationController
-  def index
-    people = PersonSerializer.new.serialize(Person.all)
-    render json: {data: people}
-  end
+  
+    def index
+         people = PersonSerializer.new.serialize(Person.all)
+        render json: {data: people}
+    end
+
+    def show
+    end
+
 end
--- spec/api/people_index_spec.rb
+++ spec/api/people_index_spec.rb
@@ -1,6 +1,6 @@
 require 'rails_helper'
 
-describe "People Index" do
+describe "People" do
   it "can get all people" do
     person1 = Person.create(first: 'max', last: 'mitrani')
     person2 = Person.create(first: 'ben', last: 'ericson')
@@ -14,4 +14,19 @@ describe "People Index" do
     people = JSON.parse(response.body)
     expect(people['data'].count).to eq(5)
   end
+  it "can get one person" do 
+    person1 = Person.create(first: 'max', last: 'mitrani')
+    person2 = Person.create(first: 'ben', last: 'ericson')
+    person3 = Person.create(first: 'tyler', last: 'billy')
+    person4 = Person.create(first: 'john', last: 'gerno')
+    person5 = Person.create(first: 'katie', last: 'rella')
+
+    get "/api/person/#{person3.last}/#{person3.first}"
+
+    expect(response).to be_successful
+    person = JSON.parse(response.body)
+    expect(person['data']['id']).to eq(person3.id)
+    expect(person['data']['first']).to eq(person3.first)
+    expect(person['data']['last']).to eq(person3.last)
+
 end
